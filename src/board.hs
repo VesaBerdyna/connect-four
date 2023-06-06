dbf1c94db7760c14d3bf381afc0137c8e6a4497e
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2023-06-06 23:15:32 +0200

    Update board.hs
     Create hole at position x y and draw piece for player i.

--- src/board.hs
+++ src/board.hs
@@ -30,5 +30,29 @@ drawPiece2 :: Float -> Float -> Picture
 drawPiece2 x y = translate x' y' piece2
   where (x', y') = getHolePos x y
 
+-- |Create hole at position x y
+drawHole :: Float -> Float -> Picture
+drawHole x y = pictures [translate x' y' hole,
+                         color empty $ translate (x' - 20) (y' + 37) $
+                         scale 0.1 0.1 $ text txt]
+  where
+    (x', y') = getHolePos x y
+    txt = "(" ++ show (round x) ++ ", " ++ show (round y) ++ ")"
+    
+
+-- |Draw piece for player i, act as a wrapper for drawPiece1/drawPiece2
+drawPiece :: Int -> Int -> Int -> Picture
+drawPiece i x y
+  | i == 1 = drawPiece1 x' y'
+  | i == 2 = drawPiece2 x' y'
+  | otherwise = blank
+  where
+    x' = fromIntegral x
+    y' = fromIntegral y
+
+-- |Draw multiple pieces
+drawPieces :: [[Int]] -> Picture
+drawPieces b = pictures [drawPiece (b !! x !! y) x y |
+                          x <- [0..6], y <- [0..5]]
 
 
