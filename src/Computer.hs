94667ce4019e7c26b56f6b5bf665fd4248bfa01c
Vesa Berdyna <vesaberdyna@gmail.com>
2023-06-10 21:02:03 +0200

    Update Computer.hs

--- src/Computer.hs
+++ src/Computer.hs
@@ -11,3 +11,15 @@ lineStreak n k i z (x:xs)
   | x == i && x == z = lineStreak n (k-1) i x xs
   | x == i && k == n = lineStreak n (k-1) i x xs
   | otherwise        = lineStreak n n i x xs
+
+verticalStreak :: Int -> Int -> [[Int]] -> Int
+verticalStreak n i = foldl (\acc x -> acc + lineStreak n n i 0 x) 0
+
+horizontalStreak :: Int -> Int -> [[Int]] -> Int
+horizontalStreak n i = verticalStreak n i . transpose
+
+diag1Streak :: Int -> Int -> [[Int]] -> Int
+diag1Streak n i = verticalStreak n i . diagonals
+
+diag2Streak :: Int -> Int -> [[Int]] -> Int
+diag2Streak n i = verticalStreak n i . diagonals . reverse
